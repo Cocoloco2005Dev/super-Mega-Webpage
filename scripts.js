2037916db143d8ac06bff29c11376d3269e3e545
CocolocoDev <cocoloco2005go@gmail.com>
2023-11-15 01:47:11 -0300

    modificacion del body, gracias juli por comentar el codigo fue un easy fix

--- scripts.js
+++ scripts.js
@@ -2,3 +2,20 @@ function abrirFormulario() {
     var nuevaVentana = window.open("form.html", "Nueva ventana", "width=600,height=550");
 }
 
+function ajustarDesplazamiento(elementId) {
+    console.log("TEST EXECUTED");
+    var headerHeight = document.querySelector('header').offsetHeight;
+    console.log(headerHeight);
+    var element = document.getElementById(elementId);
+    console.log(element);
+  
+
+    if (element) {
+      var offsetTop = element.offsetTop - headerHeight;
+      console.log(offsetTop);
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  }
